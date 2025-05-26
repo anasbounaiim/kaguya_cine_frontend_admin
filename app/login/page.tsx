21b571725e11402cd85f7547cc566d5ea3560630
achraf00007 <achrafelmardi473@gmail.com>
2025-05-26 16:36:56 +0100

    Feat: Add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/ui/login-form";
+
+
+export default function LoginPage() {
+  return (
+    <div className="bg-gradient-to-br from-red-700 from-10% via-red-900 via-30% to-red-950 flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
