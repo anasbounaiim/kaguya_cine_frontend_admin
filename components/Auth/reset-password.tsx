39609916d0bbdf767c23d9062724e136966c77a3
achraf00007 <achrafelmardi473@gmail.com>
2025-06-10 21:22:55 +0100

    feat: add reset password confirmation form and update validation schema

--- components/Auth/reset-password.tsx
+++ components/Auth/reset-password.tsx
@@ -20,9 +20,7 @@ import toast from "react-hot-toast"
 import api from "@/utils/apiFetch"
 import { ResetPasswordFormSchema } from "@/validators/resetPassword"
 import { useState } from "react"
-import { Check, Mail } from "lucide-react"
-import { ResetPasswordTokenFormSchema } from "@/validators/resetPasswordToken"
-import Link from "next/link"
+import { Mail } from "lucide-react"
 
 
 export function ResetPasswordForm({
@@ -55,41 +53,6 @@ export function ResetPasswordForm({
         }
       })
       setStatus("check")
-      // router.push("/login")
-    } catch {
-      toast.error("Error",{
-        duration: 5000,
-        style: {
-          border: '1px solid #f87171',
-          background: '#fee2e2',
-          color: '#b91c1c',
-        }
-      })
-    }
-  }
-
-  const formResetPassword = useForm<z.infer<typeof ResetPasswordTokenFormSchema>>({
-    resolver: zodResolver(ResetPasswordTokenFormSchema),
-    defaultValues: {
-      token: "",
-      newPassword: ""
-    },
-  })
-
-  async function resetPassword(values: z.infer<typeof ResetPasswordTokenFormSchema>) {
-    try {
-      const response = await api.post('/api/auth/reset-password/confirm', values)
-
-      console.log("Password has been successfully reset!:", response.data)
-      toast.success("Password has been successfully reset! ",{
-        duration: 5000,
-        style: {
-          border: '1px solid #4ade80',
-          background: '#ecfdf5',
-          color: '#065f46',
-        }
-      })
-      setStatus("success")
       // router.push("/login")
     } catch {
       toast.error("Error",{
@@ -150,80 +113,20 @@ export function ResetPasswordForm({
           )}
 
           {status === "check" && (
-            <Form {...formResetPassword}>
-                <form onSubmit={formResetPassword.handleSubmit(resetPassword)} className="p-6 md:p-8">
-                    <div className="flex flex-col gap-10">
-                      <div className="flex flex-col items-center text-center border-0 border-b border-white/30 pb-5">
-                        <div className="bg-red-50 p-4 rounded-full mb-4">
-                          <Mail className="h-12 w-12 text-red-700" />
-                        </div>
-                        <h1 className="text-2xl font-bold">Check your email</h1>
-                        <p className="text-sm text-white/80 mt-2">
-                          we sent you an email with a token to reset your password.
-                        </p>
-                      </div>
-
-                      <div className="flex flex-col justify-between gap-8">
-                        <FormField
-                          control={formResetPassword.control}
-                          name="token"
-                          render={({ field }) => (
-                            <FormItem className="w-full">
-                              <FormLabel>Token</FormLabel>
-                              <FormControl>
-                                <Input placeholder="Enter your token" {...field} className="bg-white text-black" />
-                              </FormControl>
-                              <FormMessage className="text-red-400" />
-                            </FormItem>
-                          )}
-                        />
-
-                        <FormField
-                          control={formResetPassword.control}
-                          name="newPassword"
-                          render={({ field }) => (
-                            <FormItem className="w-full">
-                              <FormLabel>New Password</FormLabel>
-                              <FormControl>
-                                <Input placeholder="***************" {...field} className="bg-white text-black" />
-                              </FormControl>
-                              <FormMessage className="text-red-400" />
-                            </FormItem>
-                          )}
-                        />
-                      </div>
-
-                      <div className="flex flex-col justify-center items-center">
-                        <Button type="submit" className=" bg-red-700 hover:bg-red-800 cursor-pointer font-extrabold w-fit px-16">
-                          Reset Password
-                        </Button>
-                      </div>
-
-                    </div>
-                </form>
-            </Form>
-          )}
-
-          {status === "success" && (
-            <div className="flex flex-col items-center text-center p-6 md:p-8">
-              <div className="bg-green-50 p-4 rounded-full mb-4">
-                <Check className="h-12 w-12 text-green-700" />
-              </div>
-              <h1 className="text-2xl font-bold">Success!</h1>
-              <p className="text-sm text-white/80 mt-2">
-                Your password has been successfully reset. You can now login with your new password.
-              </p>
-
-              <div className="flex flex-col justify-center items-center mt-6">
-                <Link href="/login" className=" bg-red-700 hover:bg-red-800 cursor-pointer font-extrabold w-fit rounded-md py-2 px-10">
-                  Login
-                </Link>
+            <div className="p-6 md:p-8">
+              <div className="flex flex-col gap-10">
+                <div className="flex flex-col items-center text-center pb-5">
+                  <div className="bg-red-50 p-4 rounded-full mb-4">
+                    <Mail className="h-12 w-12 text-red-700" />
+                  </div>
+                  <h1 className="text-2xl font-bold">Check your email</h1>
+                  <p className="text-sm text-white/80 mt-2">
+                    we sent you an email with a token to reset your password.
+                  </p>
+                </div>
               </div>
             </div>
-            
           )}
-
-
         </CardContent>
       </Card>
       <div className="text-balance text-center text-sm text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
